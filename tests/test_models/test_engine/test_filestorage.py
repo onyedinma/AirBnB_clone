49b0055feb63fe9623e5271c1df9541e3563c668
KachKreative <onyedinmaonyekachi@yahoo.com>
2023-03-12 22:40:20 +0100

    test_engine

---
+++ tests/test_models/test_engine/test_filestorage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Unit tests for City class.
+
+"""
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+from models.city import City
+from models import storage
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Test cases for fileStorage
+    """
+    testCase = BaseModel()
+
+    def test_class_name(self):
+        pass
